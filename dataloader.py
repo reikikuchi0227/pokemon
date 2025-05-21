4e3deb4615222ee17549676a51bace4c2e2fd1c8
rei.kikuchi <kiku.rei18rikou3@stu.kanazawa-u.ac.jp>
2025-05-21 19:17:18 +0900

    Fix typo in dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -27,15 +27,32 @@ plt.rcParams['font.size'] = 14
 plt.rcParams['figure.figsize'] = (6,6)
 
 # デフォルトで方眼表示ON
-plt.rcParams['axes.grud'] = True
+plt.rcParams['axes.grid'] = True
 
 # numpyの表示桁数設定
 np.set_printoptions(suppress=True, precision=5)
 
 # GPUチェック
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
 test_transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224)
+    transforms.Resize(224,224),
+    transforms.CenterCrop(224,224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5,0.5])
     ])
+
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.Resize(224,224),
+    transforms.CenterCrop(224,224),
+    transforms.ToTensor(),
+    transforms.Normalize(0.5,0.5),
+    transforms.RandomErasing(p=0.5, scale=(9.02, 0.33), ratio=(0.3, 3.3), value=0, inplace=False)
+])
+
+train_dataset = datasets.ImageFolder(root='pokemon_dataset', transform=train_transform)
+val_dataset = datasets.ImageFolder(root='pokemon_dataset', transform=test_transform)
+
+train_dataloader = 
